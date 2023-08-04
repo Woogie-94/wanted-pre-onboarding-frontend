75f5a223ddf90c8b63e7f337951ed21ab77f2447
Woogie-94 <dmsdmr9411@gmail.com>
2023-08-04 22:38:27 +0900

    add: main 페이지 구현

--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Main = () => {
-  return <>main</>;
+  return (
+    <>
+      <Link to="/signin">로그인</Link>
+      <Link to="/signup">회원가입</Link>
+    </>
+  );
 };
 
 export default Main;
