be0472140cdaa81d4131530b0fa3fcccec476897
Woogie-94 <dmsdmr9411@gmail.com>
2023-08-06 07:50:46 +0900

    add: httpService interceptors를 이용해 access_token 관리

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -3,4 +3,22 @@ import axios from "axios";
 const httpService = axios.create();
 httpService.defaults.baseURL = "https://www.pre-onboarding-selection-task.shop";
 
+httpService.interceptors.request.use(config => {
+  const accessToken = localStorage.getItem("accessToken");
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
+
+httpService.interceptors.response.use(response => {
+  if (response.data.access_token) {
+    localStorage.setItem("accessToken", response.data.access_token);
+  }
+
+  return response;
+});
+
 export default httpService;
