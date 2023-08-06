4466a9b6885011cdc8eb33292ed72874702538df
Woogie-94 <dmsdmr9411@gmail.com>
2023-08-06 21:30:48 +0900

    add: 로그인 여부에 따라 path를 redirection 하는 usePageAccess hook 구현

---
+++ src/constants/Path.ts
@@ -0,0 +1,7 @@
+export const PATH_ROOT = "/";
+export const PATH_SIGNUP = "/signup";
+export const PATH_SIGNIN = "/signin";
+export const PATH_TODO = "/todo";
+
+export const PUBLIC_PAGE_PATH = [PATH_ROOT, PATH_SIGNIN, PATH_SIGNUP];
+export const PRIVATE_PAGE_PATH = [PATH_TODO];
