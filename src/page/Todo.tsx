5fd693b3ccfe7c057141d868c9bd031ab98e3e3a
Woogie-94 <dmsdmr9411@gmail.com>
2023-08-07 21:53:49 +0900

    add: Todo 추가 기능 구현

--- src/page/Todo.tsx
+++ src/page/Todo.tsx
@@ -1,13 +1,24 @@
 import { useEffect } from "react";
 
 import TodoItem from "../component/todo/TodoItem";
+import useAddTodoSend from "../fetch/useAddTodoSend";
 import useTodoFetch from "../fetch/useTodoFetch";
+import useForm from "../hook/useForm";
 import useHttpError from "../hook/useHttpError";
 import usePageAccess from "../hook/usePageAccess";
+import { TodoFrom } from "../services/todo";
 
 const Todo = () => {
   const { data: todos, error } = useTodoFetch();
+  const { send: addTodo } = useAddTodoSend();
   const { httpError, getHttpError } = useHttpError();
+  const { register, onSubmit } = useForm<TodoFrom>({
+    initialValue: { todo: "" },
+  });
+
+  const handleAddTodo = (value: TodoFrom) => {
+    addTodo(value, { onError: getHttpError });
+  };
 
   useEffect(() => {
     getHttpError(error);
@@ -17,6 +28,10 @@ const Todo = () => {
 
   return (
     <>
+      <form onSubmit={onSubmit(handleAddTodo)}>
+        <input data-testid="new-todo-input" {...register("todo")} />
+        <button data-testid="new-todo-add-button">추가</button>
+      </form>
       <ul>{todos?.map(todo => <TodoItem key={todo.id} todo={todo} />)}</ul>
       {httpError && <p>{httpError.message}</p>}
     </>
